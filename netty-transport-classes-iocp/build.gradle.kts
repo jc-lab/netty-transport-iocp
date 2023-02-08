5426b42d7eb2b5c143d314989b5e92015c01d5da
unknown <joseph@jc-lab.net>
2023-02-08 18:59:14 +0900

    chore: maven publish script

--- netty-transport-classes-iocp/build.gradle.kts
+++ netty-transport-classes-iocp/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    id("java")
+    `java-library`
+    `maven-publish`
+    `signing`
 }
 
 repositories {
@@ -10,10 +12,63 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 
-    implementation("io.netty:netty-buffer:4.1.87.Final")
-    implementation("io.netty:netty-transport:4.1.87.Final")
+    compileOnly("io.netty:netty-buffer:4.1.87.Final")
+    compileOnly("io.netty:netty-transport:4.1.87.Final")
 }
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                name.set(project.name)
+                description.set("netty-transport-iocp")
+                url.set("https://github.com/jc-lab/netty-transport-iocp")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("jclab")
+                        name.set("Joseph Lee")
+                        email.set("joseph@jc-lab.net")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/jc-lab/netty-transport-iocp.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/jc-lab/netty-transport-iocp.git")
+                    url.set("https://github.com/jc-lab/netty-transport-iocp")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            url = uri(if ("$version".endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+            credentials {
+                username = findProperty("ossrhUsername") as String?
+                password = findProperty("ossrhPassword") as String?
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
+}
+
+tasks.withType<Sign>().configureEach {
+    onlyIf { project.hasProperty("signing.gnupg.keyName") || project.hasProperty("signing.keyId") }
 }
--- netty-transport-native-iocp/build.gradle.kts
+++ netty-transport-native-iocp/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    id("java-library")
+    `java-library`
+    `maven-publish`
+    `signing`
 }
 
 val nativeDep by configurations.creating {
@@ -20,7 +22,6 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 
     // https://mvnrepository.com/artifact/io.netty/netty-jni-util
-//    implementation("io.netty:netty-jni-util:0.0.6.Final:sources")
     nativeDep("io.netty:netty-jni-util:0.0.6.Final:sources")
 
     testImplementation("io.netty:netty-buffer:4.1.87.Final")
@@ -28,57 +29,69 @@ dependencies {
     testImplementation(project(":netty-transport-classes-iocp"))
 }
 
-val nativeSourceDir = layout.projectDirectory.dir("src/main/c")
-val nativeDepsDir = layout.buildDirectory.dir("native-deps")
-
-tasks.register<Copy>("resolveNativeDep") {
-    shouldRunAfter("dependencies")
-    dependsOn(configurations["compileClasspath"])
-    from(zipTree(nativeDep.singleFile))
-    into(nativeDepsDir)
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
 }
 
-tasks.create("buildNativeWinX86_32") {
-    shouldRunAfter("resolveNativeDep")
-    dependsOn("build")
-    val workingDirectory = layout.buildDirectory.dir("native-build-windows-x86_32").get()
-    mkdir(workingDirectory)
-    doLast {
-        ProcessBuilder()
-            .command("${layout.projectDirectory}/build-native.bat", "Win32", nativeDepsDir.get().toString(), nativeSourceDir.toString())
-            .directory(file(workingDirectory))
-            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
-            .redirectError(ProcessBuilder.Redirect.INHERIT)
-            .start()
-            .waitFor(600, TimeUnit.SECONDS)
+//val nativeSourceDir = layout.projectDirectory.dir("src/main/c")
+//val nativeDepsDir = layout.buildDirectory.dir("native-deps")
+//
+//tasks.register<Copy>("resolveNativeDep") {
+//    shouldRunAfter("dependencies")
+//    dependsOn(configurations["compileClasspath"])
+//    from(zipTree(nativeDep.singleFile))
+//    into(nativeDepsDir)
+//}
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                name.set(project.name)
+                description.set("netty-transport-iocp")
+                url.set("https://github.com/jc-lab/netty-transport-iocp")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("jclab")
+                        name.set("Joseph Lee")
+                        email.set("joseph@jc-lab.net")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/jc-lab/netty-transport-iocp.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/jc-lab/netty-transport-iocp.git")
+                    url.set("https://github.com/jc-lab/netty-transport-iocp")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            url = uri(if ("$version".endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+            credentials {
+                username = findProperty("ossrhUsername") as String?
+                password = findProperty("ossrhPassword") as String?
+            }
+        }
     }
 }
 
-tasks.create("buildNativeWinX86_64") {
-    shouldRunAfter("resolveNativeDep")
-    dependsOn("build")
-    val workingDirectory = layout.buildDirectory.dir("native-build-windows-x86_64").get()
-    mkdir(workingDirectory)
-    doLast {
-        ProcessBuilder()
-            .command("${layout.projectDirectory}/build-native.bat", "Win32", nativeDepsDir.get().toString(), nativeSourceDir.toString())
-            .directory(file(workingDirectory))
-            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
-            .redirectError(ProcessBuilder.Redirect.INHERIT)
-            .start()
-            .waitFor(600, TimeUnit.SECONDS)
-    }
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
 }
 
-//tasks.getByName("compileCpp") {
-//    dependsOn("processResources")
-//}
-
-//tasks.getByName<Test>("test") {
-//    println("LIBRARY PATH: ${layout.buildDirectory.dir("libs/main/x86-64/").get().toString()}")
-//    systemProperty("java.library.path", layout.buildDirectory.dir("libs/main/x86-64/").get().toString())
-//}
-
-//application {
-//    applicationDefaultJvmArgs = ["-Djava.library.path=" + layout.buildDirectory.dir("libs/main/x86-64/").get().toString()]
-//}
+tasks.withType<Sign>().configureEach {
+    onlyIf { project.hasProperty("signing.gnupg.keyName") || project.hasProperty("signing.keyId") }
+}
